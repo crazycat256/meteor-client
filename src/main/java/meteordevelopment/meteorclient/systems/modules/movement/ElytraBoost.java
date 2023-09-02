c90773c4804fc21d70f84c249a554f10453ed6fd
crazycat256 <63475640+crazycat256@users.noreply.github.com>
2023-09-02 13:49:04 +0200

    ElytraBoost improvements

--- src/main/java/meteordevelopment/meteorclient/systems/modules/movement/ElytraBoost.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/movement/ElytraBoost.java
@@ -14,6 +14,7 @@ import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.misc.Keybind;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.projectile.FireworkRocketEntity;
 import net.minecraft.item.FireworkRocketItem;
 import net.minecraft.item.ItemStack;
@@ -21,6 +22,7 @@ import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.Vec3d;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,6 +60,31 @@ public class ElytraBoost extends Module {
         .build()
     );
 
+    private final Setting<Boolean> controls = sgGeneral.add(new BoolSetting.Builder()
+        .name("controls")
+        .description("Allows you to boost you with the controls.")
+        .defaultValue(false)
+        .build()
+    );
+
+    private final Setting<Double> speed = sgGeneral.add(new DoubleSetting.Builder()
+        .name("speed")
+        .description("The speed to boost you with the controls.")
+        .defaultValue(1.7)
+        .sliderRange(0, 10)
+        .visible(controls::get)
+        .build()
+    );
+
+    private final Setting<Double> maneuverability = sgGeneral.add(new DoubleSetting.Builder()
+        .name("maneuverability")
+        .description("The maneuverability of the elytra.")
+        .defaultValue(0.025)
+        .sliderRange(0, 1)
+        .visible(controls::get)
+        .build()
+    );
+
     private final List<FireworkRocketEntity> fireworks = new ArrayList<>();
 
     public ElytraBoost() {
@@ -83,6 +110,64 @@ public class ElytraBoost extends Module {
     @EventHandler
     private void onTick(TickEvent.Post event) {
         fireworks.removeIf(Entity::isRemoved);
+
+        if (mc.player != null && mc.player.isFallFlying() && controls.get()) {
+            Vec3d direction = mc.player.getRotationVector();
+            Vec3d velocity = mc.player.getVelocity();
+            double length = velocity.length();
+            float yaw = mc.player.getYaw();
+            double multiplier = speed.get() * maneuverability.get();
+
+            if (mc.options.forwardKey.isPressed()) {
+                velocity = velocity.add(direction.multiply(multiplier));
+            }
+            if (mc.options.backKey.isPressed()) {
+                velocity = velocity.add(direction.multiply(-multiplier));
+            }
+            if (mc.options.leftKey.isPressed()) {
+                velocity = velocity.add(new Vec3d(Math.cos(Math.toRadians(yaw)), 0, Math.sin(Math.toRadians(yaw))).multiply(multiplier));
+            }
+            if (mc.options.rightKey.isPressed()) {
+                velocity = velocity.add(new Vec3d(Math.cos(Math.toRadians(yaw + 180)), 0, Math.sin(Math.toRadians(yaw - 90))).multiply(multiplier));
+            }
+            if (mc.options.jumpKey.isPressed()) {
+                velocity = velocity.add(0, multiplier, 0);
+            }
+            if (mc.options.sneakKey.isPressed()) {
+                velocity = velocity.add(0, -multiplier, 0);
+            }
+
+            if (velocity.length() > speed.get()) {
+                velocity = velocity.normalize().multiply(length);
+            }
+            mc.player.setVelocity(velocity);
+        }
+
+        /*if (controls.get() && mc.player != null && mc.player.getPose() == EntityPose.FALL_FLYING) {
+            Vec3d direction = mc.player.getRotationVector();
+            float yaw = mc.player.getYaw();
+            double multiplier = speed.get() * 0.05;
+
+            if (mc.options.forwardKey.isPressed()) {
+                mc.player.addVelocity(direction.multiply(multiplier));
+            }
+            if (mc.options.backKey.isPressed()) {
+                mc.player.addVelocity(direction.multiply(-multiplier));
+            }
+            if (mc.options.leftKey.isPressed()) {
+                mc.player.addVelocity(new Vec3d(Math.cos(Math.toRadians(yaw + 90)), 0, Math.sin(Math.toRadians(yaw + 90))).multiply(multiplier));
+            }
+            if (mc.options.rightKey.isPressed()) {
+                mc.player.addVelocity(new Vec3d(Math.cos(Math.toRadians(yaw - 90)), 0, Math.sin(Math.toRadians(yaw - 90))).multiply(multiplier));
+            }
+            if (mc.options.jumpKey.isPressed()) {
+                mc.player.addVelocity(0, multiplier, 0);
+            }
+            if (mc.options.sneakKey.isPressed()) {
+                mc.player.addVelocity(0, -multiplier, 0);
+            }
+        }
+        */
     }
 
     private void boost() {
